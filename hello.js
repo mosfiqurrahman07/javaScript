57eec457580610641e8d6e9788c60ff884e6c010
nisho786 <mushfiq.f2ed@gmail.com>
2020-03-16 01:48:46 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,43 @@
+console.log(12);
+
+var bananaPrice = 5;
+console.log(bananaPrice);
+
+var laptopPrice = 65000;
+console.log(laptopPrice);
+
+var motorcyclePrice = 200000;
+console.log(motorcyclePrice);
+
+var carPrice = 2500000;
+console.log(typeof carPrice);
+
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = "mosfiqur rahman";
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isRich);
+
+var homePrice = 1000000000;
+console.log(homePrice);
+
+var height = 56;
+console.log(typeof height);
+
+var goodMorning = "good morning everyone";
+console.log(goodMorning);
+
+var productItem = "mobile laptop motorcycle, car house";
+console.log(typeof productItem);
+
+var bestHp = true;
+var nonBest = false;
+console.log(nonBest);
+
+var bestMobile = true;
+var bestChinaMobile =false;
+console.log(typeof bestMobile);
